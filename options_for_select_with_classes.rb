c8c6042877a6281fb20f7c33343a04cadd2857ae
Dave Donahue <ddonahue99@gmail.com>
2011-04-25 22:49:46 -0500

    Removed old file

--- options_for_select_with_classes.rb
+++
@@ -1,7 +0,0 @@
-def options_for_select_with_classes(collection, value, label, cssclass)
-  collection.collect do |object|
-    option = [object.send(label), object.send(value)]
-    option << { :class => object.send(cssclass) } if object.send(cssclass).present?
-    option
-  end
-end
